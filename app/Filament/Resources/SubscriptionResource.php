1b0b5be03170b7869bfcba146a67c7bcf2fd9916
Hazjel <duojhs222@gmail.com>
2025-10-18 11:10:18 +0700

    CRUD Subscription

--- app/Filament/Resources/SubscriptionResource.php
+++ app/Filament/Resources/SubscriptionResource.php
@@ -101,7 +101,6 @@ class SubscriptionResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
